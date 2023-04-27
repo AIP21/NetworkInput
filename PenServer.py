feb4377e23d5d7bb7b47890945447735a9d146e3
Alexander IP <44927160+AIP21@users.noreply.github.com>
2023-04-27 18:50:06 -0400

    make exe

--- PenServer.py
+++ PenServer.py
@@ -1,15 +1,24 @@
+import os
+os.environ['KIVY_NO_CONSOLELOG'] = '1'
+
 import socket
 import pyautogui
 import time
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.label import Label
 from kivy.config import Config
 from kivy.core.window import Window
+import threading
 
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
-class InputWidget(Widget):
+class InputWidget(Label):
     clientSocket = None
+    
+    def __init__(self, hostIP, hostPort, **kwargs):
+        Label.__init__(self, text = "Host IP: " + hostIP + "     Host Port: " + hostPort, **kwargs)
+        pass
     
     def touchInfo(self, touch):
         info = str(touch.pos[0] / self.width) + "," + str(touch.pos[1] / self.height) + "," + str(touch.button) + "," + str(touch.device) + "," + str(touch.is_mouse_scrolling) + "," + str(touch.is_double_tap) + "," + str(touch.is_triple_tap) + ","
@@ -27,7 +36,7 @@ class InputWidget(Widget):
         
         data = "Move:" + str(pos[0] / self.width) + "," + str(pos[1] / self.height) + ",None,mouseHover,False,False,False,None" + "\n"
     
-        clientSocket.send(data.encode('utf-8'))
+        self.clientSocket.send(data.encode('utf-8'))
     
     def on_touch_down(self, touch):
         if (self.clientSocket == None):
@@ -35,7 +44,7 @@ class InputWidget(Widget):
         
         data = "Down:" + self.touchInfo(touch) + "\n"
         
-        clientSocket.send(data.encode('utf-8'))
+        self.clientSocket.send(data.encode('utf-8'))
         
     def on_touch_move(self, touch):
         if (self.clientSocket == None):
@@ -43,7 +52,7 @@ class InputWidget(Widget):
         
         data = "Move:" + self.touchInfo(touch) + "\n"
         
-        clientSocket.send(data.encode('utf-8'))
+        self.clientSocket.send(data.encode('utf-8'))
     
     def on_touch_up(self, touch):
         if (self.clientSocket == None):
@@ -51,59 +60,49 @@ class InputWidget(Widget):
         
         data = "Up:" + self.touchInfo(touch) + "\n"
         
-        clientSocket.send(data.encode('utf-8'))
+        self.clientSocket.send(data.encode('utf-8'))
 
 class PenInputApp(App):
-
+    clientSocket = None
+    stopping = False
+    
     def build(self):        
-        self.inputWidget = InputWidget()
+        self.host = str(socket.gethostbyname(socket.gethostname()))
+        self.port = 9090
+        self.inputWidget = InputWidget(hostIP = self.host, hostPort = str(self.port))
         
         Window.bind(mouse_pos = self.inputWidget.on_mouse_pos)
         
-        return self.inputWidget
-
-    def on_start(self):
-        host = input("Enter this device's ip address: ")
-        port = 8080
+        self.waitThread = threading.Thread(target = self.waitConnect)
+        self.waitThread.start()
         
+        return self.inputWidget
+        
+    def on_stop(self):
+        if self.clientSocket != None:
+            self.clientSocket.close()
+        
+        self.stopping = True
+    
+    def waitConnect(self):
         s = socket.socket()
-        s.bind((host, port))
+        s.bind((self.host, self.port))
         
         print("Waiting for client to connect")
         
         s.listen(1)
         
-        global clientSocket
-        clientSocket, addr = s.accept()
+        self.clientSocket, addr = s.accept()
         
         # Get connection info (client ip, port, client name)
-        info = clientSocket.getpeername()
+        info = self.clientSocket.getpeername()
         clientName = socket.getfqdn(info[0])
-                
+        
         print("Connection from: " + str(info) + ", " + str(clientName))
         
-        self.inputWidget.clientSocket = clientSocket
+        self.inputWidget.clientSocket = self.clientSocket
         
         print("Client is ready. Starting to send input data")
 
-    def on_stop(self):
-        global clientSocket
-        clientSocket.close()
-
 if __name__ == "__main__":
     PenInputApp().run()
-
-
-# data = clientSocket.recv(1024).decode('utf-8')
-
-# if not data:
-#     break
-
-# print('From online user: ' + data)
-
-# if data == "stop":
-#     print("Recieved stop message. Closing connection.")
-#     quitMessage = "disconnected by server"
-#     clientSocket.send(quitMessage.encode('utf-8'))
-#     running = False
-#     break
